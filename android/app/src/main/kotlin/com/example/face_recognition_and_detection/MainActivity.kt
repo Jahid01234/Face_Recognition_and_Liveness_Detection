cc636f3d4d21bd3fc6bd9827fa8c68b0f5a3755c
Jahid01234 <mdjahidhasan790523@gmail.com>
2026-01-11 17:04:16 +0600

    - initial project setup.......

---
+++ android/app/src/main/kotlin/com/example/face_recognition_and_detection/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.face_recognition_and_detection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:face_recognition_and_detection/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
